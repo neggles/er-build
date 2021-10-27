cd1d0373006e08dd76c883549a9ef2d9b88b23d5
neg2led <4232981+neg2led@users.noreply.github.com>
2021-10-27 13:37:47 +1100

    updating docker things and gitignore

--- docker/env-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set up environment
-export ARCH
-export CROSS_COMPILE
-export KERNELDIR
-
-alias make-module='make -j$(nproc) ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE KERNELDIR=$KERNELDIR -C $KERNELDIR M=$(pwd) modules'
-
